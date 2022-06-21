f7f81e854aad0790b17f1ef5ad88bac6a4388049
Santhosh Kumar Arumugasamy <106304716+wd00711@users.noreply.github.com>
2022-06-21 12:38:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu for Morning')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
